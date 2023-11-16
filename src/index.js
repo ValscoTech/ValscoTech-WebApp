5a22aaba6d23b76c1bedf1d7a41e7d4a8377e409
ValscoTech <valscotech@gmail.com>
2023-11-16 15:32:20 +0530

    Rendering Bug Fixed

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
-import { hydrate, render } from "react-dom";
 import { AppProvider } from "./Context/BlogContext";
 
-const root = document.getElementById("root");
-if (root.hasChildNodes()) {
-  hydrate(
-    <AppProvider>
-      <React.StrictMode>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </React.StrictMode>
-    </AppProvider>,
-    root
-  );
-} else {
-  render(
-    <AppProvider>
-      <React.StrictMode>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </React.StrictMode>
-    </AppProvider>,
-    root
-  );
-}
-
-if (module.hot) {
-  module.hot.accept();
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AppProvider>
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+</AppProvider>
+);
